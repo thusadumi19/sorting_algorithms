182af56fad7dd9db33d1eb696bd1a878f6e852ce
thusadumi19 <dumisanithusabantu@gmail.com>
2023-04-24 14:26:42 -0700

    3rd

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,36 +1,37 @@
 #include "sort.h"
-
+void swap(int *a, int *b);
 /**
- * bubble_sort - sorts an array of integers in ascending order using a bubble
- * sort algorithm
- * @array: array of integers to be sorted
- * @size: amount of elements in array
+ * bubble_sort - sort an array of integer in ascending order
+ * @array: the array to order and print
+ * @size: size of array to order
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, n;
-	int temp;
-	bool swapped = true;
+	unsigned int i = 0, j = 0;
 
-	if (!array || size < 2)
+	if (size <= 1)
 		return;
 
-	n = size;
-	while (swapped)
-	{
-		swapped = false;
-		for (i = 1; i < n; i++)
-		{
-			if (array[i - 1] > array[i])
+	for (i = 0; i < (size - 1); i++)
+		for (j = 1; j < size; j++)
+			if (array[j - 1] > array[j])
 			{
-				temp = array[i - 1];
-				array[i - 1] = array[i];
-				array[i] = temp;
-				swapped = true;
+				swap(&array[j - 1], &array[j]);
 				print_array(array, size);
 			}
-		}
-		/* optimize by logically elinating final swap from last loop */
-		n--;
-	}
-}}
+}
+
+/**
+ * swap - swap two pointers to integer
+ *
+ * @a: first int pointer
+ * @b: second int pointer
+ */
+void swap(int *a, int *b)
+{
+	int swap;
+
+	swap = *a;
+	*a = *b;
+	*b = swap;
+}
